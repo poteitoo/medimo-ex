6f17d33bcb0904ce7838e07bed317995e4e85f35
poteitoo <1105tigery@gmail.com>
2024-01-07 16:26:15 +0900

    first commit

---
+++ src/pages/Popup.tsx
@@ -0,0 +1,9 @@
+import "./Popup.css";
+
+export  default function  () {
+  return (
+      <div className="flex flex-col items-center my-5">
+      <button className="btn btn-primary btn-wide">録音を開始する</button>
+      </div>
+  )
+}
---
+++ src/pages/table.tsx
@@ -0,0 +1,37 @@
+export default function()  {
+    return<div className="overflow-x-auto mx-5">
+    <table className="table table-zebra">
+      {/* head */}
+      <thead>
+        <tr>
+          <th></th>
+          <th>日時</th>
+          <th>時間</th>
+          <th>タイトル</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* row 1 */}
+        <tr>
+          <th>1</th>
+          <td>Cy Ganderton</td>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+        </tr>
+        {/* row 2 */}
+        <tr>
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+        {/* row 3 */}
+        <tr>
+          <th>3</th>
+          <td>Brice Swyre</td>
+          <td>Tax Accountant</td>
+          <td>Red</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>}
